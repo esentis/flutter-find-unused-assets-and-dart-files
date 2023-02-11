8cc8287e2b2458c8d6147495fba3cff3d8d0b929
esentis <52862600+esentis@users.noreply.github.com>
2023-02-11 23:40:50 +0200

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Introduction
+
+This extension allows you to find unused assets and dart files in your project. Simply open the command palette (Cmd + Shift + P or Ctrl + Shift + P) and run the Find Unused Dart Files & Assets command. The output will be displayed in the Output panel of Visual Studio Code.
+
+## Usage
+
+- Open your project in Visual Studio Code.
+- Open the Command Palette (Cmd + Shift + P or Ctrl + Shift + P).
+- Type Find Unused Assets or Find Unused Dart Files and select the corresponding command from the list.
+- The results will be displayed in the Output panel.
+
+## Tips
+
+If you have multiple projects open in Visual Studio Code, make sure to switch to the correct one before running the command.
+The results may take a few seconds to appear, so be patient.
+
+We hope this extension helps you in cleaning up your projects and improving the performance. Enjoy!
---
+++ src/extension.ts
@@ -0,0 +1,94 @@
+import { exec } from "child_process";
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "unused-assets.findUnusedAssets",
+    () => {
+      let channel = vscode.window.createOutputChannel("Unreferenced Assets");
+
+      const rootPath = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
+      if (!rootPath) {
+        console.error("No workspace is open.");
+        return;
+      }
+      const assetsPath = `${rootPath}/assets/`;
+      const libPath = `${rootPath}/lib/`;
+
+      const shellFindUnusedAssets = `find "${assetsPath}" -type f -name "*.png" -o -name "*.jpeg" -o -name "*.webp" -o -name "*.gif" -o -name "*.json" -o -name "*.jpg" -print0 | while read -d $'\\0' file; do
+      name="$(basename "$file")"
+      grep -rn -F -q "$name" "${libPath}"
+      if [ $? -ne 0 ]; then
+        echo "Unreferenced asset: $file"
+      fi
+    done`;
+
+      vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: "Searching for unused assets...",
+          cancellable: true,
+        },
+        (progress, token) => {
+          return new Promise((resolve, reject) => {
+            exec(shellFindUnusedAssets, (error, stdout, stderr) => {
+              if (error) {
+                console.error(`exec error: ${error}`);
+                reject();
+                return;
+              }
+
+              if (token.isCancellationRequested) {
+                reject();
+                return;
+              }
+
+              channel.appendLine(stdout);
+              channel.show();
+              resolve(progress);
+            });
+          });
+        }
+      );
+
+      const shellFindUnusedDartFiles = `cd "${rootPath}" && 
+            find lib/ -name "*.dart" -print0 | while read -d $'\\0' file; do
+        name="$(basename "$file")"
+        grep -rn -F -q "$name" lib/
+        if [ $? -ne 0 ]; then
+          echo "Unreferenced file: $file"
+        fi
+      done`;
+
+      vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: "Searching for unused dart files...",
+          cancellable: true,
+        },
+        (progress, token) => {
+          return new Promise((resolve, reject) => {
+            exec(shellFindUnusedDartFiles, (error, stdout, stderr) => {
+              if (error) {
+                console.error(`exec error: ${error}`);
+                reject();
+                return;
+              }
+
+              if (token.isCancellationRequested) {
+                reject();
+                return;
+              }
+
+              channel.appendLine(stdout);
+              channel.show();
+              resolve(progress);
+            });
+          });
+        }
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
